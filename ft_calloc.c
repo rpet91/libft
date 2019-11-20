3accea7b4896f4279c9e9ee48551736a85fc6cfb
rpet91 <57190868+rpet91@users.noreply.github.com>
2019-11-20 15:46:17 +0100

    Add files via upload

---
+++ ft_calloc.c
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        ::::::::            */
+/*   ft_calloc.c                                        :+:    :+:            */
+/*                                                     +:+                    */
+/*   By: rpet <marvin@codam.nl>                       +#+                     */
+/*                                                   +#+                      */
+/*   Created: 2019/10/31 09:22:07 by rpet          #+#    #+#                 */
+/*   Updated: 2019/11/13 10:34:48 by rpet          ########   odam.nl         */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdlib.h>
+#include "libft.h"
+
+void	*ft_calloc(size_t count, size_t size)
+{
+	void			*ptr;
+	unsigned char	*res;
+	size_t			i;
+
+	i = 0;
+	ptr = malloc(count * size);
+	if (ptr == NULL)
+		return (NULL);
+	res = (unsigned char*)ptr;
+	ft_bzero(res, (count * size));
+	return (ptr);
+}
